0b64c20f395183f77d0011d2e39a2e73cf0d84d7
Omer <farukoncu78@gmail.com>
2024-03-29 00:17:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Container from "react-bootstrap/Container";
+import Header from "./components/header/Header";
+import LegendContainer from "./components/legends/LegendContainer";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+      <LegendContainer />
+    
+    </Container>
+
+  );
+}
+
+export default App;
